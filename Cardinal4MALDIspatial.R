b0637594a3f458052fd1be41429f8c680fd837ec
raulmejia <r8a8m8p8@gmail.com>
2021-10-27 16:26:36 +0200

    installing cardinal prerrequisites

--- Cardinal4MALDIspatial.R
+++ Cardinal4MALDIspatial.R
@@ -30,6 +30,51 @@ if (!require("Cardinal")) {
   BiocManager::install("Cardinal", ask =FALSE)
   library("Cardinal")
 }
+# apt-get install libtiff-dev
+if (!require("tiff")) {
+  BiocManager::install("tiff", ask =FALSE)
+  library("tiff")
+}
+if (!require("fftwtools")) {
+  BiocManager::install("fftwtools", ask =FALSE)
+  library("fftwtools")
+} ## Error :/ 
+
+
+# tiff, fftwtools, EBImage , Cardinal
+
+
+############################## 
+## Data given by the user
+##############################
+# create parser object
+parser <- ArgumentParser()
+# specify our desired options 
+# by default ArgumentParser will add an help option 
+parser$add_argument("-v", "--verbose", action="store_true", default=TRUE,
+                    help="Print extra output [default]")
+parser$add_argument("-q", "--quietly", action="store_false", 
+                    dest="verbose", help="Print little output")
+parser$add_argument("-m", "--matrix", type="character", 
+                    help="path to your expression matrix")
+parser$add_argument("-a", "--annotation", type="character", 
+                    help="path to your annotation file")
+parser$add_argument("-c", "--code", type="character", 
+                    help="path to your code")
+parser$add_argument("-l", "--label", type="character", 
+                    help="label to your results")
+parser$add_argument("-g", "--maingroups", type="character", 
+                    help="the name of your column to correct / make intrabatch normalization")
+parser$add_argument("-s", "--selectedgenesfromrownamesofadataframe", type="character", 
+                    help="A data frame that contains the genes that you want to plot in the row names, only the rownames will be used for that tash")
+parser$add_argument("-o", "--outputfolder", type="character", 
+                    help="output folder where you want to store your results")
+
+# get command line options, if help option encountered print help and exit,
+# otherwise if options not found on command line then set defaults, 
+args <- parser$parse_args( )
+# print some progress messages to stderr if "quietly" wasn't requested
+
 ########################################
 #### data given by the user
 #########################################
